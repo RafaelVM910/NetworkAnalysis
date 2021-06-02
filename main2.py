9d1fdc15c497f06aa7543ef6e7ff1b60ff6a139f
RafaelVM910 <64099691+RafaelVM910@users.noreply.github.com>
2021-06-02 01:33:34 -0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -8,17 +8,23 @@ import json
 warnings.filterwarnings('ignore')
 
 g = nx.Graph()
-nodes = 20
+nodes = 100000
 edges = nodes * 5
-weeks = 4
+infectar = round(nodes * 0.05)
+weeks = 52
+
+Sx = []
+Ix = []
+Rx = []
+Mx = []
+graphY = []
 
 # States: 0 = S, 1 = I, 2 = R, 3 = M
+# Colors: b = S, r = I, g = R, w = M
 
-# Crear nodos
 for i in range(nodes):
     g.add_node(i, state=0, days=0)
 
-# Crear vertices
 for i in range(edges):
     x = random.randint(0, nodes-1)
     y = random.randint(0, nodes-1)
@@ -30,16 +36,37 @@ for i in range(edges):
 
     g.add_edge(x, y)
 
-# Asegurar vertices para todos
 for i in range(nodes):
     if len(g[i]) == 0:
         g.add_edge(i, (i + random.randint(1, nodes - 1)) % edges)
+        edges += 1
 
-# Infectar nodo
-g.nodes[random.randint(0, nodes-1)]['state'] = 1
+for i in range(infectar):
+    g.nodes[random.randint(0, nodes-1)]['state'] = 1
 
-# Simulacion
 for days in range(weeks * 7):
+    # Guardar informacion
+    susceptibles = 0
+    infectados = 0
+    recuperados = 0
+    muertos = 0
+
+    for i in range(nodes):
+        if g.nodes(data='state')[i] == 0:
+            susceptibles += 1
+        elif g.nodes(data='state')[i] == 1:
+            infectados += 1
+        elif g.nodes(data='state')[i] == 2:
+            recuperados += 1
+        elif g.nodes(data='state')[i] == 3:
+            muertos += 1
+
+    Sx.append(susceptibles)
+    Ix.append(infectados)
+    Rx.append(recuperados)
+    Mx.append(muertos)
+    graphY.append(days)
+
     # Invulnerabilizar
     for i in range(nodes):
         if g.nodes(data='state')[i] == 2 and g.nodes[i]['days'] == 7:
@@ -51,12 +78,12 @@ for days in range(weeks * 7):
         if g.nodes(data='state')[i] == 1:
             for j in list(g.edges):
                 if j[0] == i and g.nodes(data='state')[j[1]] == 0:
-                    if random.randint(0, 100) < 50:
+                    if random.randint(0, 100) < 60:
                         g.nodes[j[1]]['state'] = 1
-                        g.nodes[j[1]]['days'] = 0
+                        g.nodes[i]['days'] = 0
 
             x = random.randint(0, 100)
-            if 2 < x <= 15:
+            if 2 < x <= 10:
                 g.nodes[i]['state'] = 2
                 g.nodes[i]['days'] = 0
 
@@ -73,12 +100,30 @@ for days in range(weeks * 7):
         g.nodes[i]['days'] += 1
 
 
-# Dibujar grafo
+# Grafo simulacion
 pos = nx.spring_layout(g)
 for i in range(nodes):
-    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 0], node_color='b')
-    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 1], node_color='r')
-    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 2], node_color='g')
-    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 3], node_color='grey')
-nx.draw_networkx_edges(g, pos, width=1.0, alpha=0.5, edge_color='k')
+    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 0], node_color='b', node_size=7)
+    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 1], node_color='r', node_size=7)
+    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 2], node_color='g', node_size=7)
+    nx.draw_networkx_nodes(g, pos, [x for x in g.nodes() if g.nodes(data='state')[x] == 3], node_color='grey', node_size=7)
+nx.draw_networkx_edges(g, pos, width=0.2, alpha=0.5, edge_color='k')
 plt.show()
+
+# Grafica Personas
+plt.plot(graphY, Sx, color='b', label='Susceptibles')
+plt.plot(graphY, Ix, color='r', label='Infectados')
+plt.plot(graphY, Rx, color='g', label='Recuperados')
+plt.plot(graphY, Mx, color='k', label='Muertos')
+plt.xlabel('Dias')
+plt.ylabel('Personas')
+plt.title('Estados a traves del tiempo')
+plt.legend()
+plt.show()
+
+
+with open('Data.txt', 'a') as file:
+    for i in range(len(Sx)):
+        save = {"dia": i, "susceptibles": Sx[i], "infectados": Ix[i], "recuperados": Rx[i], "muertos": Mx[i]}
+        json.dump(save, file, indent=4)
+    file.close()
